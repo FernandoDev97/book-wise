24fb75dc3e81ca528905e9a518a50cd7459c5a22
Fernando Souza <l.fernando.dev@gmail.com>
2023-04-27 21:02:13 -0300

    up project

---
+++ src/pages/_app.tsx
@@ -0,0 +1,11 @@
+import { globalStyles } from '@/styles/global'
+import type { AppProps } from 'next/app'
+import { Nunito } from 'next/font/google'
+
+export const nunito = Nunito({ subsets: ['latin'] })
+
+globalStyles()
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
