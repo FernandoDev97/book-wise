7ae88e0416cc2eed94b24a0e0a935f1cea833444
Fernando Souza <l.fernando.dev@gmail.com>
2023-05-13 21:15:51 -0300

    add rules prisma

--- src/components/AuthButtons/index.tsx
+++ src/components/AuthButtons/index.tsx
@@ -1,18 +1,36 @@
-import { AuthButton, Container } from './styles';
+import { signIn } from "next-auth/react";
+import { AuthButton, Container } from "./styles";
+import { useRouter } from "next/router";
 
-export function AuthButtons() {
+type AuthButtonsProps = {
+  callbackUrl?: string;
+};
+
+export function AuthButtons({ callbackUrl = "/" }: AuthButtonsProps) {
+  const router = useRouter();
+
+  const handleSingnIn = (provider?: string) => {
+    if (!provider) {
+      router.push(callbackUrl);
+      return;
+    }
+    signIn(provider, {
+      callbackUrl,
+    });
+  };
+
   return (
     <Container>
-      <AuthButton>
+      <AuthButton onClick={() => handleSingnIn("google")}>
         <img src="/images/icons/google.svg" alt="Google Logo" />
         Entrar com Google
       </AuthButton>
-      <AuthButton>
-      <img src="/images/icons/github.svg" alt="Github Logo" />
+      <AuthButton onClick={() => handleSingnIn("github")}>
+        <img src="/images/icons/github.svg" alt="Github Logo" />
         Entrar com Github
       </AuthButton>
-      <AuthButton>
-      <img src="/images/icons/rocket.svg" alt="Rocket Logo" />
+      <AuthButton onClick={() => handleSingnIn()}>
+        <img src="/images/icons/rocket.svg" alt="Rocket Logo" />
         Entrar como visitante
       </AuthButton>
     </Container>
