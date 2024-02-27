244ef9d23f0a0b736bf881fdfe7eb80793136022
FernandoDev97 <l.fernando.dev@gmail.com>
2024-02-26 21:25:48 -0300

    fix bugs and console

--- src/app/(pages)/explore/_components/explore-books.tsx
+++ src/app/(pages)/explore/_components/explore-books.tsx
@@ -72,7 +72,7 @@ export const ExploreBooks = ({ books, categories }: ExploreBooksProps) => {
         </div>
         <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 ">
           {filteredBooks.map((book) => (
-            <BookItem key={book.id} book={book} />
+            <BookItem key={book.id} currentBook={book} />
           ))}
         </div>
       </div>
