71bd005a4f200b933850fe9509c4dfd5de555f46
FernandoDev97 <l.fernando.dev@gmail.com>
2024-02-26 21:25:23 -0300

    fix call to back end with react query

--- src/app/(pages)/explore/_components/book-item.tsx
+++ src/app/(pages)/explore/_components/book-item.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import { BookWithAvgRating, RatingWithAuthor } from '@/@types/types-prisma'
 import { RatingStars } from '@/components/common/rating-stars'
 import { Button } from '@/components/ui/button'
@@ -12,11 +10,13 @@ import {
 } from '@/components/ui/sheet'
 import { BookOpen, Bookmark } from 'lucide-react'
 import Image from 'next/image'
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { UserRatingCard } from './user-rating-card'
 import { getBookDetails } from '../_actions/get-book-details'
 import { CategoriesOnBooks, Category } from '@prisma/client'
 import { RatingForm } from './rating-form'
+import { useQuery } from '@tanstack/react-query'
+import { useSession } from 'next-auth/react'
 
 type BookDetails = BookWithAvgRating & {
   ratings: RatingWithAuthor[]
@@ -26,41 +26,51 @@ type BookDetails = BookWithAvgRating & {
 }
 
 interface BookItemProps {
-  book: BookWithAvgRating
+  currentBook: BookWithAvgRating
 }
 
-export const BookItem = ({ book }: BookItemProps) => {
+export const BookItem = ({ currentBook }: BookItemProps) => {
   const [open, setOpen] = useState(false)
   const [showForm, setShowForm] = useState(false)
-  const [bookDetails, setBooksDetails] = useState<BookDetails>()
+  const { data: session } = useSession()
 
-  useEffect(() => {
-    async function bookDatails() {
-      if (open) {
-        const { book: bookDetail } = await getBookDetails(book.id)
-        setBooksDetails(bookDetail)
-      }
-    }
+  const { data: bookDetails } = useQuery<BookDetails>({
+    queryKey: ['book', currentBook.id],
+    queryFn: async () => {
+      const data = await getBookDetails(currentBook.id)
+      const { book } = data
 
-    bookDatails()
-  }, [open, book.id])
+      return book
+    },
+    enabled: open,
+  })
 
   const categories = bookDetails?.categories
     .map((item) => item.category.name)
     .join(', ')
 
+  const ratings = bookDetails?.ratings.map((rating) => rating.rate)
+
+  const sortedRatingByDate = bookDetails?.ratings.sort((a, b) => {
+    return new Date(b.created_at).getTime() - new Date(a.created_at).getTime()
+  })
+
+  const canRate = bookDetails?.ratings.every(
+    (item) => item.user_id !== session?.user.id,
+  )
+
   return (
     <Sheet open={open} onOpenChange={setOpen}>
       <SheetTrigger asChild>
         <button className="w-full overflow-hidden relative bg-gray-700 py-4 px-5 rounded-lg h-auto flex gap-5">
-          {book.alreadyRead && (
-            <div className="absolute uppercase rounded-l-sm bottom-0 text-green-100 font-bold text-xs right-0 px-3 py-1 bg-[#0a313c]">
+          {currentBook.alreadyRead && (
+            <div className="absolute uppercase rounded-l-sm top-0 text-green-100 font-bold text-xs right-0 px-3 py-1 bg-[#0a313c]">
               <span>Lido</span>
             </div>
           )}
           <Image
-            src={book?.cover_url}
-            alt={book?.name}
+            src={currentBook?.cover_url}
+            alt={currentBook?.name}
             width={0}
             height={0}
             sizes="100vh"
@@ -69,14 +79,14 @@ export const BookItem = ({ book }: BookItemProps) => {
           <div className="flex flex-col h-full justify-between">
             <div className="flex flex-col">
               <p className="text-base font-bold overflow-hidden text-start text-ellipsis line-clamp-2">
-                {book?.name}
+                {currentBook?.name}
               </p>
               <p className="text-sm text-gray-400 text-start overflow-hidden text-ellipsis line-clamp-1">
-                {book?.author}
+                {currentBook?.author}
               </p>
             </div>
 
-            <RatingStars rating={book.avgRating} className="mt-auto" />
+            <RatingStars rating={currentBook.avgRating} className="mt-auto" />
           </div>
         </button>
       </SheetTrigger>
@@ -86,8 +96,8 @@ export const BookItem = ({ book }: BookItemProps) => {
             <main className="w-full h-[415px] bg-gray-700 px-8 py-6 rounded-[10px] mt-6 flex flex-col">
               <section className="w-full h-[242px] flex gap-8 ">
                 <Image
-                  src={book?.cover_url}
-                  alt={book?.name}
+                  src={currentBook?.cover_url}
+                  alt={currentBook?.name}
                   width={0}
                   height={0}
                   sizes="100vh"
@@ -106,13 +116,13 @@ export const BookItem = ({ book }: BookItemProps) => {
                   <div className="flex flex-col gap-1">
                     <RatingStars
                       size={20}
-                      rating={book.avgRating}
+                      rating={currentBook.avgRating}
                       className="mt-auto"
                     />
                     <span className="text-sm text-gray-400">
-                      {String(book.ratings) === '1'
-                        ? `${String(book.ratings)} avaliação`
-                        : `${String(book.ratings)} avaliações`}{' '}
+                      {String(ratings?.length) === '1'
+                        ? `${String(ratings?.length)} avaliação`
+                        : `${String(ratings?.length)} avaliações`}{' '}
                     </span>
                   </div>
                 </div>
@@ -131,7 +141,7 @@ export const BookItem = ({ book }: BookItemProps) => {
                   <div>
                     <span className="text-gray-300 text-sm">Páginas</span>
                     <h3 className="text-base font-bold">
-                      {String(book.total_pages)}
+                      {String(currentBook.total_pages)}
                     </h3>
                   </div>
                 </div>
@@ -143,7 +153,7 @@ export const BookItem = ({ book }: BookItemProps) => {
             <main className="w-full flex flex-col gap-4">
               <div className="flex w-full justify-between ">
                 <span>Avaliações</span>
-                {!showForm && (
+                {!showForm && canRate && (
                   <Button
                     onClick={() => setShowForm(!showForm)}
                     className="rounded text-purple-100 font-bold hover:bg-purple-100/10"
@@ -158,7 +168,7 @@ export const BookItem = ({ book }: BookItemProps) => {
                   cancelShowForm={() => setShowForm(!showForm)}
                 />
               )}
-              {bookDetails?.ratings.map((rating) => (
+              {sortedRatingByDate?.map((rating) => (
                 <UserRatingCard key={rating.id} rating={rating} />
               ))}
             </main>
