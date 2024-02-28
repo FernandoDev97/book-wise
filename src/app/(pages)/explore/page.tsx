5caed8ed1c8c3dfee120dab0ed1e563ae7b02130
FernandoDev97 <l.fernando.dev@gmail.com>
2024-02-27 23:20:11 -0300

    add function to get search params for explore page

--- src/app/(pages)/explore/page.tsx
+++ src/app/(pages)/explore/page.tsx
@@ -3,7 +3,13 @@ import { prismaClient } from '@/lib/prisma'
 import React from 'react'
 import { findCategoriesBooks } from './_actions/find-categories-books'
 
-const Explore = async () => {
+interface ExplorePageProps {
+  searchParams: {
+    bookId?: string
+  }
+}
+
+const ExplorePage = async ({ searchParams }: ExplorePageProps) => {
   const [{ books }, categories] = await Promise.all([
     await findCategoriesBooks(null),
     await prismaClient.category.findMany(),
@@ -12,10 +18,14 @@ const Explore = async () => {
   return (
     <main className="w-full h-full flex flex-col pr-5 overflow-auto pb-5 no-scrollbar">
       <section className="mt-10 flex gap-3 w-full">
-        <ExploreBooks books={books} categories={categories} />
+        <ExploreBooks
+          bookId={searchParams.bookId}
+          books={books}
+          categories={categories}
+        />
       </section>
     </main>
   )
 }
 
-export default Explore
+export default ExplorePage
