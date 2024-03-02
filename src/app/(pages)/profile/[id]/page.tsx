b8ff95af05c9ace1c46c027bc760bcb11419ee01
FernandoDev97 <l.fernando.dev@gmail.com>
2024-03-02 00:06:29 -0300

    fix profile page

--- src/app/(pages)/profile/[id]/page.tsx
+++ src/app/(pages)/profile/[id]/page.tsx
@@ -3,14 +3,13 @@ import { PageTitle } from '@/components/common/page-title'
 import { getServerSession } from 'next-auth'
 import React from 'react'
 import { ButtonBack } from './_components/button-back'
-import { Input } from '@/components/ui/input'
-import { Search, User } from 'lucide-react'
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { getProfile } from './_actions/get-profile'
-import { Rating } from '@prisma/client'
 import { redirect } from 'next/navigation'
+import { RecentRatings } from './_components/recent-ratings'
+import { RatingWithBookAndCategories } from './_components/recent-rating-card'
+import { ProfileDetalis } from './_components/profile-detalis'
 
-interface ProfileDataTypes {
+export interface ProfileDataTypes {
   profile: {
     user: {
       id: string
@@ -18,10 +17,10 @@ interface ProfileDataTypes {
       name: string
       member_since: string
     }
-    ratings: Rating[]
+    ratings: RatingWithBookAndCategories[]
     readPages: number
     ratedBooks: number
-    readAuthor: number
+    readAuthors: number
     mostReadCategory?: string
   }
 }
@@ -49,36 +48,11 @@ const ProfilePage = async ({ params }: ProfilePageProps) => {
           ) : (
             <ButtonBack />
           )}
-          <form className="w-full relative focus-within:border-green-200 focus-within:text-green-200 border border-gray-500 rounded transition-all">
-            <Input
-              placeholder="Buscar livro ou autor"
-              className="bg-transparent px-5 text-gray-400 py-6 outline-none focus:outline-none border-none"
-            />
-            <Search
-              size={20}
-              className="absolute text-gray-500 right-5 bottom-3.5"
-            />
-          </form>
+
+          <RecentRatings ratings={profile.ratings} />
         </section>
 
-        <section className="col-span-1 flex items-center flex-col border-l border-solid h-fit border-gray-700 gap-5 mt-20">
-          <Avatar className="w-[72px] h-[72px]">
-            <AvatarImage
-              className="object-cover"
-              width={72}
-              height={72}
-              src={profile?.user?.image}
-              alt={`Foto do perfil de ${profile?.user?.name}`}
-            />
-            <AvatarFallback>
-              <User size={72} />
-            </AvatarFallback>
-          </Avatar>
-          <div className="flex flex-col gap-0.5 items-center">
-            <p className="text-xl font-bold ">{profile?.user?.name}</p>
-            <p className="text-sm text-gray-400">membro desde 2019</p>
-          </div>
-        </section>
+        <ProfileDetalis profile={profile} />
       </div>
     </main>
   )
