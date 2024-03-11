577024ee5fa4fd100db3c253f48cb764b70e6e78
FernandoDev97 <l.fernando.dev@gmail.com>
2024-03-10 22:45:31 -0300

    add try catch to server action

--- src/app/(pages)/(home)/_actions/popular-books.ts
+++ src/app/(pages)/(home)/_actions/popular-books.ts
@@ -4,43 +4,47 @@
 import { prismaClient } from '@/lib/prisma'
 
 export async function popularBooks(params?: string) {
-  const take = Number(params)
-  const books = await prismaClient.book.findMany({
-    orderBy: {
-      ratings: {
-        _count: 'desc',
+  try {
+    const take = Number(params)
+    const books = await prismaClient.book.findMany({
+      orderBy: {
+        ratings: {
+          _count: 'desc',
+        },
       },
-    },
-    include: {
-      ratings: true,
-    },
-    take: take || 4,
-  })
-
-  const booksAvgRating = await prismaClient.rating.groupBy({
-    by: ['book_id'],
-    where: {
-      book_id: {
-        in: books.map((book) => book.id),
+      include: {
+        ratings: true,
       },
-    },
-    _avg: {
-      rate: true,
-    },
-  })
+      take: take || 4,
+    })
 
-  const booksWithAvgRating = books.map((book) => {
-    const bookAvgRating = booksAvgRating.find(
-      (avgRating) => avgRating.book_id === book.id,
-    )
-    const { ratings, ...bookInfos } = book
+    const booksAvgRating = await prismaClient.rating.groupBy({
+      by: ['book_id'],
+      where: {
+        book_id: {
+          in: books.map((book) => book.id),
+        },
+      },
+      _avg: {
+        rate: true,
+      },
+    })
+
+    const booksWithAvgRating = books.map((book) => {
+      const bookAvgRating = booksAvgRating.find(
+        (avgRating) => avgRating.book_id === book.id,
+      )
+      const { ratings, ...bookInfos } = book
+      return {
+        ...bookInfos,
+        avgRating: bookAvgRating?._avg.rate,
+      }
+    })
+
     return {
-      ...bookInfos,
-      avgRating: bookAvgRating?._avg.rate,
+      books: booksWithAvgRating,
     }
-  })
-
-  return {
-    books: booksWithAvgRating,
+  } catch (error) {
+    console.error(error)
   }
 }
--- src/app/(pages)/(home)/_actions/user-latest-rating.ts
+++ src/app/(pages)/(home)/_actions/user-latest-rating.ts
@@ -3,17 +3,21 @@
 import { prismaClient } from '@/lib/prisma'
 
 export const getUserLatestRating = async (userId: string) => {
-  const latestUserRating = await prismaClient.rating.findFirst({
-    where: {
-      user_id: userId,
-    },
-    orderBy: {
-      created_at: 'desc',
-    },
-    include: {
-      book: true,
-    },
-  })
+  try {
+    const latestUserRating = await prismaClient.rating.findFirst({
+      where: {
+        user_id: userId,
+      },
+      orderBy: {
+        created_at: 'desc',
+      },
+      include: {
+        book: true,
+      },
+    })
 
-  return latestUserRating
+    return latestUserRating
+  } catch (error) {
+    console.error(error)
+  }
 }
